55997453987c5310939fa86166c09c30a7ac0ff8
nnnnicholas <nnnnicholas@users.noreply.github.com>
2019-08-24 16:54:45 -0400

    Created twilio.js to test SMS. NB: removed account ID and Auth Key. Have not yet figured out how to safely load secrets so must insert  by hand for now.

---
+++ twilio.js
@@ -0,0 +1,14 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+// DANGER! This is insecure. See http://twil.io/secure
+const accountSid = 'ACCOUNT SID';
+const authToken = 'AUTH TOKEN';
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+15145008028',
+     to: '+15149462453'
+   })
+  .then(message => console.log(message.sid));
